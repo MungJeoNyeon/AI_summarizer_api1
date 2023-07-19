c5b23558ad53fa0d774395719a5501e1f1d5ec35
jounghyunMoon <moonjhyun0613@gmail.com>
2023-07-19 19:15:34 +0900

    api_commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+      긴 기사를 명확하고 간결한 요약으로 변환하는 오픈 소스 기사 요약 기능인
+      Summize로 읽기를 단순화합니다
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
